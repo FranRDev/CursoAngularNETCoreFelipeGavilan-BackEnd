0f3e831373ea8580d9ed3f44233355bbd2142b87
Francisco Rodríguez <franciscorodriguezgar@gmail.com>
2021-01-15 18:24:01 +0100

    Agregar archivos de proyecto.

---
+++ Entidades/Genero.cs
@@ -0,0 +1,10 @@
+﻿namespace back_end.Entidades {
+
+    public class Genero {
+
+        public int ID { get; set; }
+        public string Nombre { get; set; }
+
+    }
+
+}
---
+++ Repositorios/RepositorioEnMemoria.cs
@@ -0,0 +1,23 @@
+﻿using back_end.Entidades;
+using System.Collections.Generic;
+
+namespace back_end.Repositorios {
+
+    public class RepositorioEnMemoria {
+
+        private List<Genero> generos;
+
+        public RepositorioEnMemoria() {
+            generos = new List<Genero>() {
+                new Genero() { ID = 1, Nombre = "Comedia"},
+                new Genero() { ID = 2, Nombre = "Acción"}
+            };
+        }
+
+        public List<Genero> ObtenerGeneros() {
+            return generos;
+        }
+
+    }
+
+}
